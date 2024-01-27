3fae271974370ba11f0b53b4ab66407d5e66d9c6
Giovanni Souza <GSMartins999>
2024-01-26 23:48:31 -0300

    type

--- src/tipando/index.ts
+++ src/tipando/index.ts
@@ -1,11 +1,16 @@
-function buscarCarrosPorMarca(frota, marca) {
+import { TCarro, frota } from "./frota"
+
+//Para voltar uma marca opcional podemos apenas colocar um ? na frente, assim como em marca.
+function buscarCarrosPorMarca(frota: TCarro[], marca?: string): TCarro[] {
   if (marca === undefined) {
     return frota
   }
 
   return frota.filter(
-    (carro) => {
+    (carro: TCarro) => {
       return carro.marca === marca
     }
   )
 }
+
+console.log(buscarCarrosPorMarca(frota));
